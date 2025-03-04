88f959c35974092b33efce90840611fc34840958
Erik Jonsson <fallowshades@gmail.com>
2025-03-04 10:01:08 +0100

    robots.txt

--- Timeview/client/src/components/transaction/TransactionCard.tsx
+++ Timeview/client/src/components/transaction/TransactionCard.tsx
@@ -7,7 +7,7 @@ type dynamicFooterContainer = {
 const TransactionCard = ({ children }: dynamicFooterContainer) => {
   return (
     <div className='space-y-4 flex justify-center '>
-      <div className='min-w-[1000px] mt-4  py-12  '>
+      <div className='max-w-[1000px] lg:min-w-[1000px] mt-4  py-12  '>
         <div className=''>{children}</div>
       </div>
     </div>
